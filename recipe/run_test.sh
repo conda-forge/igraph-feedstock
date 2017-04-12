5d58b6415149a0c73129942298c698e783c3591c
Vincent Gauthier <pub@luxbulb.org>
2017-04-12 18:56:45 +0200

    add osx support to the recipe

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -3,7 +3,24 @@ system=$(uname -s)
 
 case $system in
 	"Linux" )
-		gcc igraph_test.c $(pkg-config --libs --cflags igraph) -o igraph_test
+        export CC=gcc
+		${CC} igraph_test.c $(pkg-config --libs --cflags igraph) -o igraph_test
 		./igraph_test
 		;;
+    "Darwin" )
+        export CC=clang
+        export CXX=clang++
+        export MACOSX_VERSION_MIN="10.7"
+        export MACOSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}"
+        export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+        export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
+        export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+        export LDFLAGS="${LDFLAGS} -stdlib=libc++ -lc++"
+        export LINKFLAGS="${LDFLAGS}"
+        export CFLAGS="${CFLAGS} -m${ARCH}"
+        export CXXFLAGS="${CXXFLAGS} -m${ARCH}"
+        export DYLD_FALLBACK_LIBRARY_PATH=$DYLD_FALLBACK_LIBRARY_PATH:${PREFIX}/lib
+    	${CC} igraph_test.c ${CFLAGS} ${LDFLAGS} $(pkg-config --libs --cflags igraph) -o igraph_test
+    	./igraph_test
+    	;;
 esac
