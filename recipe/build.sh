72af9667bac15893abd6ccf2fb930c75987809f8
Isuru Fernando <isuruf@gmail.com>
2017-05-07 23:49:35 +0530

    Remove toolchain from run requirements and simplify build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,19 +5,7 @@ set -x -e
 export LIBIGRAPH_FALLBACK_INCLUDE_DIRS="${PREFIX}/include"
 export LIBIGRAPH_FALLBACK_LIBRARY_DIRS="${PREFIX}/lib"
 
-if [ "$(uname)" == "Darwin" ]; then
-    CC=${CC} CXX=${CXX} CFLAGS=${CFLAGS} CXXFLAGS=${CXXFLAGS} LDFLAGS=${LDFLAGS} ./configure \
-        --disable-debug \
-        --disable-dependency-tracking \
-	    --prefix=${PREFIX}
-
-    make -j $CPU_COUNT
-    make install
-fi
-
-if [ "$(uname)" == "Linux" ]; then
-    ./configure --prefix=${PREFIX}
-    make -j $CPU_COUNT
-    make check
-    make install
-fi
+./configure --prefix=${PREFIX}
+make -j $CPU_COUNT
+make check
+make install
