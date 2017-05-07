3ef9069e4a1a03d72ff3b824a12e65e8b658c04b
Isuru Fernando <isuruf@gmail.com>
2017-05-08 00:03:07 +0530

    Fix LDFLAGS so that gmp is enabled

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,9 @@ set -x -e
 export LIBIGRAPH_FALLBACK_INCLUDE_DIRS="${PREFIX}/include"
 export LIBIGRAPH_FALLBACK_LIBRARY_DIRS="${PREFIX}/lib"
 
+export CPPFLAGS="-I$PREFIX/include $CPPFLAGS"
+export LDFLAGS="-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib $LDFLAGS"
+
 ./configure --prefix=${PREFIX}
 make -j $CPU_COUNT
 make check
