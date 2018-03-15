79877657deb06ec9f826df4af21bf317f4051010
Patrick Sodre <psodre@gmail.com>
2018-03-15 06:31:29 -0400

    Possible issue for vc9

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,4 +8,6 @@ cd igraph-%PKG_VERSION%-msvc
 copy %LIBRARY_PREFIX%\include\stdint.h winclude
 
 vcbuild /upgrade igraph.vcproj
+
+rem How to pass /DPRPACK_IGRAPH_SUPPORT=1 to build command
 vcbuild igraph.vcproj "release|win32"
