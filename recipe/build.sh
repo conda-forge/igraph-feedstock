32557604c483d35acdf1a173febdfa8566e952c7
Isuru Fernando <isuruf@gmail.com>
2018-11-15 00:38:26 -0600

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,8 +12,8 @@ fi
 # fix the simple test runner which doesn't use ldflags
 export CC="${CC} ${CFLAGS} ${LDFLAGS}"
 
-./configure --prefix=${PREFIX} | sed "s|$PREFIX|<PREFIX>|g"
-make -j $CPU_COUNT
+./configure --prefix=${PREFIX}
+make -j $CPU_COUNT | sed "s|$PREFIX|<PREFIX>|g"
 (make check | sed "s|$PREFIX|<PREFIX>|g") || (cat tests/testsuite.log && exit 1)
 make install
 mv igraph.pc ${PREFIX}/lib/pkgconfig
