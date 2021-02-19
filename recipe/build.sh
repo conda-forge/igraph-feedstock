0c5143e64f93037709f92ad93d441194117db996
Vincent Traag <vincent@traag.net>
2021-02-19 14:10:30 +0100

    Explicitly set libxml2 directly for macOS.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,22 +1,41 @@
 #!/bin/env bash
 
 set -ex
+system=$(uname -s)
 
 mkdir -p build
 pushd build
 
-cmake -GNinja \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_PREFIX_PATH=$PREFIX \
-      -DCMAKE_INSTALL_LIBDIR=lib \
-      -DLibXml2_ROOT=$PREFIX \
-      -DCMAKE_INSTALL_INCLUDEDIR=include \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_C_FLAGS="$CFLAGS" \
-      -DCMAKE_POSITION_INDEPENDENT_CODE=on \
-      -DBLA_VENDOR=OpenBLAS \
-      -DBUILD_SHARED_LIBS=on \
-      ..
+case $system in
+    "Linux" )
+        cmake -GNinja \
+            -DCMAKE_BUILD_TYPE=Release \
+            -DCMAKE_PREFIX_PATH=$PREFIX \
+            -DCMAKE_INSTALL_LIBDIR=lib \
+            -DCMAKE_INSTALL_INCLUDEDIR=include \
+            -DCMAKE_INSTALL_PREFIX=$PREFIX \
+            -DCMAKE_C_FLAGS="$CFLAGS" \
+            -DCMAKE_POSITION_INDEPENDENT_CODE=on \
+            -DBLA_VENDOR=OpenBLAS \
+            -DBUILD_SHARED_LIBS=on \
+            ..
+        ;;
+    "Darwin" )
+        cmake -GNinja \
+            -DCMAKE_BUILD_TYPE=Release \
+            -DCMAKE_PREFIX_PATH=$PREFIX \
+            -DCMAKE_INSTALL_LIBDIR=lib \
+            -DCMAKE_INSTALL_INCLUDEDIR=include \
+            -DCMAKE_INSTALL_PREFIX=$PREFIX \
+            -DCMAKE_C_FLAGS="$CFLAGS" \
+            -DLIBXML2_LIBRARY=$PREFIX/lib/libxml2.dylib \
+            -DLIBXML2_INCLUDE_DIR=$PREFIX/include/libxml2/ \
+            -DCMAKE_POSITION_INDEPENDENT_CODE=on \
+            -DBLA_VENDOR=OpenBLAS \
+            -DBUILD_SHARED_LIBS=on \
+            ..
+        ;;
+esac
 
 cmake --build . --config Release --target igraph
 cmake --build . --config Release --target check
