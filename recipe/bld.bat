c4d428834d9a312a59c1e6cfbc28c1cb8d552728
Vincent Traag <vincent@traag.net>
2021-02-20 09:23:41 +0100

    Try Release with everything internal.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,17 +6,17 @@ if errorlevel 1 exit 1
 cd build
 
 cmake -GNinja ^
-      -DCMAKE_BUILD_TYPE=Debug ^
+      -DCMAKE_BUILD_TYPE=Release ^
       -DCMAKE_PREFIX_PATH=%CONDA_PREFIX% ^
       -DCMAKE_INSTALL_LIBDIR=%LIBRARY_LIB% ^
       -DCMAKE_INSTALL_INCLUDEDIR=%LIBRARY_INC% ^
       -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -DCMAKE_POSITION_INDEPENDENT_CODE=on ^
-      -DIGRAPH_USE_INTERNAL_BLAS=0 ^
-      -DIGRAPH_USE_INTERNAL_LAPACK=0 ^
+      -DIGRAPH_USE_INTERNAL_BLAS=1 ^
+      -DIGRAPH_USE_INTERNAL_LAPACK=1 ^
       -DIGRAPH_USE_INTERNAL_ARPACK=1 ^
-      -DIGRAPH_USE_INTERNAL_GLPK=0 ^
-      -DIGRAPH_USE_INTERNAL_CXSPARSE=0 ^
+      -DIGRAPH_USE_INTERNAL_GLPK=1 ^
+      -DIGRAPH_USE_INTERNAL_CXSPARSE=1 ^
       -DIGRAPH_USE_INTERNAL_GMP=1 ^
       -DBUILD_SHARED_LIBS=on ^
       -DIGRAPH_ENABLE_LTO=1 ^
@@ -24,10 +24,10 @@ cmake -GNinja ^
       -DIGRAPH_GRAPHML_SUPPORT=1 ^
       ..
 
-cmake --build . --config Debug --target igraph
-cmake --build . --config Debug --target build_tests
-ctest --progress --output-on-failure --config C Debug
+cmake --build . --config Release --target igraph
+cmake --build . --config Release --target build_tests
+ctest --progress --output-on-failure --config C Release
 if errorlevel 1 exit 1
-cmake --build . --config Debug --target install
+cmake --build . --config Release --target install
 
 if errorlevel 1 exit 1
